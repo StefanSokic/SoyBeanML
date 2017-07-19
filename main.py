87c81a0a1c06a21919729c404c591af426f482bf
StefanSokic <stefan.sokic33@gmail.com>
2017-07-19 13:05:29 -0400

    SS - plotting data and starting univariate linear regression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sklearn.model_selection as train_test_split
+from sklearn import linear_model
 import matplotlib.pyplot as plt
 
 # import data intro dataframes
@@ -22,13 +23,28 @@ print('merged_df nums', merged_df.isnull().values.any())
 clean_df = merged_df._get_numeric_data()
 print('clean df', clean_df.head(5))
 
-# try to visulalize - fails
-clean_df.plot()
-
 # 80/20 split of the data for training and testing
 train = clean_df.sample(frac=0.8, random_state=200)
 test = clean_df.drop(train.index)
 
 print('train', train.head(5))
 print('test', test.head(5))
+
+# plot the data
+# train.head(1000).plot(x='YEAR', y='YIELD', kind='scatter')
+# plt.show()
+
+# linear regression with scikit learn
+# univariate linear regression just on YEAR to start
+x_train = train['YEAR'].values.reshape(-1,1)
+y_train = train['YIELD'].values.reshape(-1,1)
+x_test = test['YEAR'].values.reshape(-1,1)
+y_test = test['YIELD'].values.reshape(-1,1)
+
+# training the model
+regr = linear_model.LinearRegression()
+regr.fit(x_train, y_train)
+
+# showing some coefficients
+print('Coefficients', regr.coef_)
 
