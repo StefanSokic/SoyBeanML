1fe7e17259f1b38d7a2ef66e52a60450ed052280
StefanSokic <stefan.sokic33@gmail.com>
2017-07-26 11:08:33 -0400

    univariate linear regression with plotting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import sklearn.model_selection as train_test_split
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
@@ -27,24 +26,15 @@ print('clean df', clean_df.head(5))
 train = clean_df.sample(frac=0.8, random_state=200)
 test = clean_df.drop(train.index)
 
-print('train', train.head(5))
-print('test', test.head(5))
-
-# plot the data,
-# train.head(1000).plot(x='YEAR', y='YIELD', kind='scatter')
+# plotting the data to get a feel for it
+# train.head(1000).plot(x='YEAR', y='TEMP_01', kind='scatter')
 # plt.show()
 
 # linear regression with scikit learn
-# univariate linear regression just on YEAR to start
-# --------- how to split train and test + use multivariate ---------------
-
-# the x_train will be matrix is the yeild col removed
+# splitting data into features and target
+# just training on the first temperature columns since this is univariate
 feature_cols = ['TEMP_01']
 target_col = ['YIELD']
-cols = list(train)
-cols.remove(target_col[0])
-feature_cols = cols
-
 x_train = train[feature_cols]
 y_train = train[target_col]
 x_test = test[feature_cols]
@@ -54,20 +44,15 @@ y_test = test[target_col]
 regr = linear_model.LinearRegression()
 regr.fit(x_train, y_train)
 
-# showing some coefficients
+# showing some results
 print('Coefficients', regr.coef_)
-
-print("Mean squared error: %.2f"
-      % np.mean((regr.predict(x_test) - y_test) ** 2))
+print("Mean squared error: %.2f" % np.mean((regr.predict(x_test) - y_test) ** 2))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % regr.score(x_test, y_test))
 
 # Plot outputs
-# plt.scatter(x_test, y_test,  color='black')
-# plt.plot(x_test, regr.predict(x_test), color='blue',
-#          linewidth=3)
-
-# plt.xticks(())
-# plt.yticks(())
-
-# plt.show()
+plt.scatter(x_test, y_test,  color='black')
+plt.plot(x_test, regr.predict(x_test), color='blue', linewidth=3)
+plt.xticks(())
+plt.yticks(())
+plt.show()
